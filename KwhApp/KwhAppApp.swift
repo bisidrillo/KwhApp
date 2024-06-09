c30736cd5de2e42205d866f7cdec969a5cd63d92
bisidrillo <bisidrillo@me.com>
2024-06-09 22:06:34 +0100

    Initial Commit

---
+++ KwhApp/KwhAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KwhAppApp.swift
+//  KwhApp
+//
+//  Created by Isidro Jose Suarez Rodriguez on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct KwhAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
